c481f70625868a6ae39bb543b5a71ac214eddb0e
Maria Holm <maria.lilleberg@gmail.com>
2022-02-18 14:02:15 +0100

    login working

--- js/ui/createMenu.js
+++ js/ui/createMenu.js
@@ -1,44 +1,38 @@
 import { getUsername } from "../utils/storage.js";
+import logoutButton from "./logout.js";
 
 export default function createMenu() {
   const { pathname } = document.location;
 
   const container = document.querySelector(".menu");
-  const loginIcon = document.querySelector(".login-icon");
+  const loginIcon = document.querySelector(".login-icon-container");
   const username = getUsername();
   console.log(username);
 
-  let authLink = `<a href="login.html" class="${
-    pathname === "/login.html" ? "active" : ""
-  }>Sign in</a>`;
-
-  loginIcon.style.display = "block";
+  let authLink = "";
 
   if (username) {
-    loginIcon.style.display = "none";
-
     authLink = `<li class="nav-item nav__item me-lg-3 ${
-      pathname === "/admin/products.html" ? "active nav__item--active" : ""
+      pathname === "/admin-products.html" ? "active nav__item--active" : ""
     }">
-      <a href="admin/products.html" class="nav-link nav__link ${
-        pathname === "/admin/products.html" ? "nav__link--active" : ""
+      <a href="admin-products.html" class="nav-link nav__link ${
+        pathname === "/admin-products.html" ? "nav__link--active" : ""
       }">
       All products</a>
     </li>
 
     <li class="nav-item nav__item ${
-      pathname === "/admin/add-product.html" ? "active nav__item--active" : ""
+      pathname === "/add-product.html" ? "active nav__item--active" : ""
     }">
-      <a href="admin/add-product.html" class="nav-link nav__link ${
-        pathname === "/admin/add-product.html" ? "nav__link--active" : ""
+      <a href="add-product.html" class="nav-link nav__link ${
+        pathname === "/add-product.html" ? "nav__link--active" : ""
       }">
       Add</a>
     </li>
-
-    <li class="nav-item nav__item">
-      <a href="../index.html" id="logout" class="nav-link nav__link">Log out</a>
-    </li>
   `;
+
+    loginIcon.innerHTML = `<button id="logout">Log out</button>`;
+    logoutButton();
   }
 
   container.innerHTML = `${authLink}`;
