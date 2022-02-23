2bb28ae5d3a6c884638f63a9213a83e728b3c193
Maria Holm <maria.lilleberg@gmail.com>
2022-02-23 01:35:48 +0100

    admin prods md-lg done

--- js/admin-products.js
+++ js/admin-products.js
@@ -3,7 +3,8 @@ import { baseUrl } from "./settings/api.js";
 
 createMenu();
 
-const container = document.querySelector(".admin-products");
+const containerMd = document.querySelector(".admin-products-md");
+const containerSm = document.querySelector(".admin-products-sm");
 const url = baseUrl + "/products";
 
 (async function () {
@@ -18,24 +19,47 @@ const url = baseUrl + "/products";
 })();
 
 function createHTML(products) {
-  container.innerHTML = "";
+  containerMd.innerHTML = "";
+  // containerSm.innerHTML = "";
 
   products.forEach((prod) => {
-    container.innerHTML += `
-    <div class="sections">
-        <p class="sections__part">Id</p>
-        <p class="sections__part></p>
-        <p class="sections__part">Title</p>
-        <p class="sections__part">Description</p>
-        <p class="sections__part">Price</p>
-      </div>
-      <div class="products__info">
-        <p>${prod.id}</p>
-        <img src="${baseUrl}${prod.image.url}" alt="${prod.image.alternativeText}">
-        <p>${prod.title}</p>
-        <p>${prod.description}</p>
-        <p>${prod.price}</p>
+    containerMd.innerHTML += `
+      <div class="info d-grid">
+        <p class="info__id">${prod.id}</p>
+        <img src="${baseUrl}${prod.image.url}" alt="${prod.image.alternativeText}" class="info__img">
+        <p class="info__title">${prod.title}</p>
+        <p class="info__desc">${prod.description}</p>
+        <p class="info__price">${prod.price}</p>
+        <div class="info__edit-del">
+          <i class="fa-solid fa-pen"></i>
+          <i class="fa-solid fa-trash-can"></i>
+        </div>
       </div>
     `;
+
+    /*containerSm.innerHTML += `
+      <div class="info info-sm d-md-none">
+        <div class="info-sm__sections">
+          <p class="sections__part">Id</p>
+          <p class="info-sm__id info-sm__elem">${prod.id}</p>
+        </div>
+        <div class="info-sm__sections">
+          <div class="sections__part"></div>
+          <img src="${baseUrl}${prod.image.url}" alt="${prod.image.alternativeText}" class="info-sm__img info-sm__elem info__img">
+        </div>
+        <div class="info-sm__sections">
+          <p class="sections__part">Title</p>
+          <p class="info-sm__title info-sm__elem">${prod.title}</p>
+        </div>
+        <div class="info-sm__sections">
+          <p class="sections__part">Description</p>
+          <p class="info-sm__desc info-sm__elem">${prod.description}</p>
+        </div>
+        <div class="info-sm__sections">
+          <p class="sections__part">Price</p>
+          <p class="info-sm__price info-sm__elem">${prod.price}</p>
+        </div>
+      </div>
+    `; */
   });
 }
